f28e4875fb6f8c3cb1ce682642159341bf0b2b66
James Kuligowski <jdkuligowski@gmail.com>
2023-01-16 10:06:59 +0000

    changing debug setting

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@l=ptekid53f1tbew3qc7%=$5tylt7x!ica9x!8y8+xfu%(_jo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['wittle-test.azurewebsites.net']
 
