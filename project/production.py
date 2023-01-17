7d58d28810be81b71a59e294cd8796ea28039255
James Kuligowski <jdkuligowski@gmail.com>
2023-01-17 20:13:29 +0000

    more test deployment settings changes

--- project/production.py
+++ project/production.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@l=ptekid53f1tbew3qc7%=$5tylt7x!ica9x!8y8+xfu%(_jo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
