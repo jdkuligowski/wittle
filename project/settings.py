68d4ba6a3fddbe5a9340320f21a153a28579d833
James Kuligowski <jdkuligowski@gmail.com>
2023-01-20 14:36:27 +0000

    build changes

--- project/settings.py
+++ project/settings.py
@@ -20,7 +20,7 @@ environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-print('Base ->', BASE_DIR)
+# print('Base ->', BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -109,7 +109,7 @@ TEMPLATES = [
     },
 ]
 
-print('templates ->', TEMPLATES)
+# print('templates ->', TEMPLATES)
 
 WSGI_APPLICATION = 'project.wsgi.application'
 
@@ -175,14 +175,14 @@ STATICFILES_DIRS = (
     # os.path.join(BASE_DIR, 'client'),
 )
 
-print('static-files ->', STATICFILES_DIRS)
+# print('static-files ->', STATICFILES_DIRS)
 
 STATIC_ROOT = (
     # os.path.join(BASE_DIR, "static")
     BASE_DIR.as_posix() + '/static'
 )
 
-print('static-root ->', STATIC_ROOT)
+# print('static-root ->', STATIC_ROOT)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
