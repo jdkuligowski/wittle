81e64038481ceae450216c17179fd868d5be7b10
James Kuligowski <jdkuligowski@gmail.com>
2023-02-17 16:58:33 +0000

    lazy loading main images

--- client/src/components/wittleSearch/resultBlocks.js/PropertyResultsWittle.js
+++ client/src/components/wittleSearch/resultBlocks.js/PropertyResultsWittle.js
@@ -17,7 +17,7 @@ import Heatmap from '../../helpers/modals/Heatmap'
 import EditSearch from '../../helpers/modals/EditSearch'
 import MatchInsights from '../../helpers/modals/MatchInsights'
 import AllPropertiesMap from '../../helpers/modals/AllPropertiesMap'
-
+import LazyLoad from 'react-lazyload'
 
 const PropertyResultsWittle = () => {
 
@@ -1487,33 +1487,34 @@ const PropertyResultsWittle = () => {
                               <h4 onClick={handleInsightShow} id={property.id}>🔥 {property.first_match}% match</h4>
                             </div>
                             <div className='slide-section-desktop'>
-
-                              <Slide className='slide-import' autoplay={false} transitionDuration={750} arrows={true} indicators={true}>
-                                {property.property_images.map((images, index) => {
-                                  return (
-                                    <>
-                                      <div className='image-card' id={property.id} style={{ backgroundImage: `url('${images.url}')` }} key={index}>
-                                        <div className='property-image-details'>
-                                          {formData.search_channel === 'Renting' ?
-                                            <h3><NumericFormat value={property.monthly} displayType={'text'} thousandSeparator={true} prefix={'£'} /> pcm</h3>
-                                            :
-                                            <h3>Fixed Price: <NumericFormat value={property.value} displayType={'text'} thousandSeparator={true} prefix={'£'} /> </h3>
-                                          }
-                                        </div>
-                                        {listFavourites ?
-                                          <div className='favourite-section' id={property.id} onClick={postFavourite}>
-                                            {listFavourites.includes(property.id) ?
-                                              <div className='favourite-button-on' id={property.id} ></div>
+                              <LazyLoad offset={100}>
+                                <Slide className='slide-import' autoplay={false} transitionDuration={750} arrows={true} indicators={true}>
+                                  {property.property_images.map((images, index) => {
+                                    return (
+                                      <>
+                                        <div className='image-card' id={property.id} style={{ backgroundImage: `url('${images.url}')` }} key={index}>
+                                          <div className='property-image-details'>
+                                            {formData.search_channel === 'Renting' ?
+                                              <h3><NumericFormat value={property.monthly} displayType={'text'} thousandSeparator={true} prefix={'£'} /> pcm</h3>
                                               :
-                                              <div className='favourite-button-off' id={property.id} ></div>
+                                              <h3>Fixed Price: <NumericFormat value={property.value} displayType={'text'} thousandSeparator={true} prefix={'£'} /> </h3>
                                             }
                                           </div>
-                                          : ''}
-                                      </div>
-                                    </>
-                                  )
-                                })}
-                              </Slide>
+                                          {listFavourites ?
+                                            <div className='favourite-section' id={property.id} onClick={postFavourite}>
+                                              {listFavourites.includes(property.id) ?
+                                                <div className='favourite-button-on' id={property.id} ></div>
+                                                :
+                                                <div className='favourite-button-off' id={property.id} ></div>
+                                              }
+                                            </div>
+                                            : ''}
+                                        </div>
+                                      </>
+                                    )
+                                  })}
+                                </Slide>
+                              </LazyLoad>
 
                             </div>
                             {/* <div className='detail-section' onClick={setID} id={property.first_match}> */}
