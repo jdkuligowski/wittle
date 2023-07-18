60edf1a5c766f98b7f000c359cd82ba62d6c1daa
James Kuligowski <jdkuligowski@gmail.com>
2023-07-18 08:03:23 +0100

    changing google auth stuff for app

--- project/settings.py
+++ project/settings.py
@@ -264,10 +264,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'accounts@wittle.co'
-EMAIL_HOST_PASSWORD = 'squzadbbeqsiqpll'
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+# EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 
 
 # LOGGING = {
