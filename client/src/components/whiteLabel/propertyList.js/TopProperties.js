a2a959597297977a47b98844c14d30945cd590bd
James Kuligowski <jdkuligowski@gmail.com>
2024-01-15 10:47:05 +0000

    grid view on wittle og

--- client/src/components/whiteLabel/propertyList.js/TopProperties.js
+++ client/src/components/whiteLabel/propertyList.js/TopProperties.js
@@ -7,6 +7,7 @@ import Loading from '../../helpers/Loading'
 import KYCInput from '../b2bModals/KYCInput'
 import ReactPaginate from 'react-paginate'
 import ReactMapGL, { Marker, Popup, Source, Layer } from 'react-map-gl'
+import { set } from 'react-ga'
 
 const TopProperties = ({ setListingSelection, fetchData }) => {
 
@@ -28,6 +29,9 @@ const TopProperties = ({ setListingSelection, fetchData }) => {
   // states for handling the popups on the map
   const [showPopup, setShowPopup] = useState(true)
   const [iconId, setIconId] = useState()
+
+  // state for detail view on property
+  const [propertyDetails, setPropertyDetails] = useState('Details')
 
   // filter array for proeprties to search
   const [propertyFilters, setPropertyFilters] = useState({
@@ -235,7 +239,19 @@ const TopProperties = ({ setListingSelection, fetchData }) => {
     }))
   }
 
-
+  const mapNumberToWord = (number) => {
+    // const value = number * 10s
+    if (number > 0.9) return 'Outstanding'
+    if (number > 0.8) return 'Excellent'
+    if (number > 0.7) return 'Very good'
+    if (number > 0.6) return 'Good'
+    if (number > 0.5) return 'Quite good'
+    if (number > 0.4) return 'Average'
+    if (number > 0.3) return 'Below average'
+    if (number > 0.2) return 'Bad'
+    if (number > 0.1) return 'Poor'
+    return 'Terrible'
+  }
 
 
   return (
@@ -261,6 +277,9 @@ const TopProperties = ({ setListingSelection, fetchData }) => {
                 </div>
                 <div className={`icon-box ${propertyViewFormat === 'Map' ? 'active' : 'inactive'}`} onClick={() => setPropertyViewFormat('Map')}>
                   <div className='icon' id='map'></div>
+                </div>
+                <div className={`icon-box ${propertyViewFormat === 'Grid' ? 'active' : 'inactive'}`} onClick={() => setPropertyViewFormat('Grid')}>
+                  <div className='icon' id='grid'></div>
                 </div>
               </div>
             </div>
@@ -371,22 +390,15 @@ const TopProperties = ({ setListingSelection, fetchData }) => {
                               </div>
                               <div className='grid-right' id={item.id} onMouseEnter={iconSetting} >
                                 <h5 className='title' onClick={() => handleVisitUrl(item.property_data.url)}>{index + 1}. {item.property_data.displayAddress}</h5>
-                                {/* <div className='details'>
-                                  <div className='icon' id='catchment'></div>
-                                  <h5>{item.rating > 4.8 ? 'Excellent' : item.rating > 4.5 ? 'Very good' : item.rating > 4.2 ? 'Good' : item.rating > 3.9 ? 'Average' : item.rating > 0 ? 'Poor' : 'N/a'}</h5>
-                                </div> */}
                                 <h5 className='sub-title'>Bedrooms: {item.property_data.bedrooms}</h5>
                                 <h5 className='sub-title'>Price: {item.property_data.price}</h5>
                                 <h5 className='sub-title'>Score: {(item.overall_lifestyle_score / 10).toFixed(2)}</h5>
                               </div>
                             </div>
                             <hr className="dividing-line" />
-
                           </>
                         )
                       }).slice(startIndex, endIndex) : ''}
-
-
                     </div>
 
                     <div className="map-section">
@@ -451,7 +463,139 @@ const TopProperties = ({ setListingSelection, fetchData }) => {
                     : ''}
                 </div>
 
-                : ''}
+                : propertyViewFormat === 'Grid' ?
+                  <div className='property-grid-view'>
+                    <div className='property-grid-items'>
+                      {properties ? properties.map((item, index) => {
+                        return (
+                          <>
+                            <div className='property-item'>
+                              <div className='property-item-left'>
+                                <div className='property-item-image' onClick={() => handleVisitUrl(item.property_data.url)} style={{ backgroundImage: `url(${item.property_data.images})` }}></div>
+                                <div className='property-core-info'>
+                                  {propertyFilters.channel === 'Sales' ?
+                                    <h3><NumericFormat value={item.property_data.price} displayType={'text'} thousandSeparator={true} prefix={'£'} /> </h3> :
+                                    <h3><NumericFormat value={item.property_data.price} displayType={'text'} thousandSeparator={true} prefix={'£'} /> pcm</h3>
+                                  }
+                                  <h3 className='match-score'>🔥 {(item.overall_lifestyle_score * 10).toFixed(0)}% match</h3>
+                                </div>
+                              </div>
+                              <div className='property-item-right'>
+                                <div className='detail-selections'>
+                                  <div className='key-selectors'>
+                                    <h3 className='detail-selector' style={{ fontWeight: propertyDetails === 'Details' ? '700' : '500' }} onClick={() => setPropertyDetails('Details')}>Details</h3>
+                                    <h3 className='detail-selector' style={{ fontWeight: propertyDetails === 'Insights' ? '700' : '500' }} onClick={() => setPropertyDetails('Insights')}>Insights</h3>
+                                    <h3 className='detail-selector' style={{ fontWeight: propertyDetails === 'Floorplan' ? '700' : '500' }} onClick={() => setPropertyDetails('Floorplan')}>Floorplan</h3>
+                                  </div>
+                                  <div className='action'>
+                                    <button onClick={() => goToListing(item)}>Explore</button>
+                                  </div>
+                                </div>
+                                {propertyDetails === 'Details' ?
+
+                                  <div className='property-body'>
+                                    <h2 className='property-title'>{item.property_data.displayAddress}, {item.persona_data_list[0].district}</h2>
+                                    <h3 className='added-details'>{item.property_data.added_revised ? `Added on ${item.property_data.added_revised}` : `Reduced on ${item.property_data.reduced_revised}`}</h3>
+                                    <div className='key-elements'>
+                                      <div className='property-element-item'>
+                                        <div className='element-icon' id='bed'></div>
+                                        <h3>{item.property_data.bedrooms} bedrooms</h3>
+                                      </div>
+                                      <div className='property-element-item'>
+                                        <div className='element-icon' id='bath'></div>
+                                        <h3>{item.property_data.bathrooms} bathrooms</h3>
+                                      </div>
+                                      <div className='property-element-item'>
+                                        <div className='element-icon' id='size'></div>
+                                        <h3>{item.property_data.size} sq. ft</h3>
+                                      </div>
+                                      <div className='property-element-item'>
+                                        <div className='element-icon' id='type'></div>
+                                        <h3>{item.property_data.propertyType}</h3>
+                                      </div>
+                                    </div>
+                                    <div className='property-features'>
+                                      <h3 className='features-title'>Features</h3>
+                                      <div className='feature-list'>
+                                        {item.property_data.features.split(', ').map((feature, index) => (
+                                          <li key={index} className='feature-item'>{feature}</li>
+                                        ))}
+                                      </div>
+                                    </div>
+                                  </div>
+                                  : propertyDetails === 'Insights' ?
+                                    <div className='property-body'>
+                                      <div className='insights-body'>
+                                        <div className='insight-row'>
+                                          <div className='insight-icon' id='primaries'></div>
+                                          <h3 className='insight'>{mapNumberToWord(item.persona_data_list[0].primary_percentile)} for primary schools</h3>
+                                        </div>
+                                        <div className='insight-row'>
+                                          <div className='insight-icon' id='secondaries'></div>
+                                          <h3 className='insight'>{mapNumberToWord(item.persona_data_list[0].secondary_percentile)} for secondary schools</h3>
+                                        </div>
+                                        <div className='insight-row'>
+                                          <div className='insight-icon' id='parks'></div>
+                                          <h3 className='insight'>{mapNumberToWord(item.persona_data_list[0].park_area_percentile)} for access to parks</h3>
+                                        </div>
+                                        <div className='insight-row'>
+                                          <div className='insight-icon' id='gyms'></div>
+                                          <h3 className='insight'>{mapNumberToWord(item.persona_data_list[0].gym_percentile)} for access to gyms</h3>
+                                        </div>
+                                        <div className='insight-row'>
+                                          <div className='insight-icon' id='pubs'></div>
+                                          <h3 className='insight'>{mapNumberToWord(item.persona_data_list[0].pub_percentile)} for access to pubs</h3>
+                                        </div>
+                                        <div className='insight-row'>
+                                          <div className='insight-icon' id='restaurants'></div>
+                                          <h3 className='insight'>{mapNumberToWord(item.persona_data_list[0].restaurant_percentile)} for restaurants</h3>
+                                        </div>
+                                        <div className='insight-row'>
+                                          <div className='insight-icon' id='crime'></div>
+                                          <h3 className='insight'>{mapNumberToWord(item.persona_data_list[0].crime_percentile)} for crime rates</h3>
+                                        </div>
+                                        <div className='insight-row'>
+                                          <div className='insight-icon' id='tubes'></div>
+                                          <h3 className='insight'>{mapNumberToWord(item.persona_data_list[0].tube_percentile)} for tube access</h3>
+                                        </div>
+                                        <div className='insight-row'>
+                                          <div className='insight-icon' id='evs'></div>
+                                          <h3 className='insight'>{mapNumberToWord(item.persona_data_list[0].ev_percentile)} for EV access</h3>
+                                        </div>
+                                      </div>
+                                    </div>
+                                    : propertyDetails === 'Floorplan' ?
+                                      <div className='property-body'>
+                                        <div className='floorplan-body' onClick={() => handleVisitUrl(item.property_data.floorplan_url)} style={{ backgroundImage: `url(${item.property_data.floorplan_url})` }}></div>
+                                      </div>
+                                      : ''}
+
+
+
+                              </div>
+                            </div>
+                          </>
+                        )
+                      }).slice(startIndex, endIndex)
+
+                        : ''}
+
+                    </div>
+                    {properties ?
+                      <ReactPaginate
+                        pageCount={Math.ceil(properties.length / 50)}
+                        onPageChange={handlePageClick}
+                        containerClassName={'pagination'}
+                        activeClassName={'active'}
+                        previousLabel={'<'}
+                        nextLabel={'>'}
+                        pageRangeDisplayed={0}
+                        breakLabel={'...'}
+                      />
+                      : ''}
+                  </div>
+
+                  : ''}
 
 
           </section>
