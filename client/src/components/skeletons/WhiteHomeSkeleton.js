d25d455a0ca2d3cfa73d185dc3bc7d2d10680660
James Kuligowski <jdkuligowski@gmail.com>
2024-02-13 10:07:23 +0000

    trying to fix favicon

---
+++ client/src/components/skeletons/WhiteHomeSkeleton.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+const WhiteHomeSkeleton = () => {
+
+
+  return (
+    <>
+
+      <section className="white-home-skeleton">
+        <section className="skeleton-sidebar">
+
+        </section>
+
+        <section className="skeleton-body">
+        
+        </section>
+      </section>
+
+    </>
+
+  )
+}
+
+export default WhiteHomeSkeleton
