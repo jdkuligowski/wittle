8f9d41aa7a6dbcb782dbcff41398075abaa9a66b
James Kuligowski <jdkuligowski@gmail.com>
2024-02-22 19:33:11 +0000

    additional homepage tweaks

--- client/src/components/NewHomepage.js
+++ client/src/components/NewHomepage.js
@@ -15,11 +15,122 @@ import leadGenImage from '../images/lead-gen-small.png'
 import insightsImage from '../images/insights-small.png'
 import aiImage from '../images/ai-generator-small.png'
 import wittleSearch from '../images/wittle-search-small.png'
+import AgentSignup from './helpers/modals/AgentSignup'
+import AgentSubmitted from './helpers/modals/AgentSubmitted'
 // import openImage from '../images/open.png'
 
 
 const NewHomepage = () => {
 
+  // track page view with google analytics
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname)
+  }, [])
+
+  // state for errors
+  const [errors, setErrors] = useState(false)
+
+  // state for submitting
+  const [agentSubmitted, setAgentSubmitted] = useState(false)
+
+  // set the state for the waitlist signup data capture
+  const [agentData, setAgentData] = useState({
+    email: '',
+    name: '',
+    company: '',
+    position: '',
+    message: '',
+  })
+
+  const [submitError, setSubmitError] = useState({
+    email: '',
+    name: '',
+    company: '',
+    position: '',
+    post: '',
+  })
+
+
+  // update registration data
+  const handleChange = (e) => {
+    setAgentData({ ...agentData, [e.target.name]: e.target.value })
+    if (e.target.name === 'name') {
+      if (e.target.value.length < 1) {
+        setSubmitError({ ...submitError, name: 'Add name' })
+      } else {
+        setSubmitError({ ...submitError, name: '' })
+      }
+
+    } else if (e.target.name === 'email') {
+      if (!isEmail(agentData.email)) {
+        setSubmitError({ ...submitError, email: 'Invalid email address' })
+      } else {
+        setSubmitError({ ...submitError, email: '' })
+      }
+
+    } else if (e.target.name === 'company') {
+      if (e.target.value.length < 1) {
+        setSubmitError({ ...submitError, company: 'Add company' })
+      } else {
+        setSubmitError({ ...submitError, company: '' })
+      }
+
+    } else if (e.target.name === 'position') {
+      if (e.target.value.length < 1) {
+        setSubmitError({ ...submitError, position: 'Add position' })
+      } else {
+        setSubmitError({ ...submitError, position: '' })
+      }
+    }
+  }
+
+  // submit email address to waitlist
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const { data } = await axios.post('/api/agentsignup/', agentData)
+      handleAgentClose()
+      setAgentSubmitted(true)
+      handleSubmitShow()
+      setAgentData(false)
+    } catch (err) {
+      setSubmitError({ ...submitError, post: 'Make sure you\'ve populated all fields before you submit' })
+    }
+
+  }
+
+
+  // manageing the modal pop up for property search
+  const [agentShow, setAgentShow] = useState(false)
+
+  // close modal
+  const handleAgentClose = () => {
+    setAgentShow(false)
+    setSubmitError(false)
+    setAgentSubmitted(false)
+    setAgentData(false)
+  }
+
+  // show the modal
+  const handleAgentShow = (e) => {
+    setAgentSubmitted(false)
+    setAgentShow(true)
+  }
+
+  // manageing modal for submitted 
+  const [agentSubmittedShow, setAgentSubmittedShow] = useState(false)
+
+  // close modal
+  const handleSubmitClose = () => {
+    setAgentSubmittedShow(false)
+    setSubmitError(false)
+    setAgentSubmitted(false)
+  }
+
+  // show the modal
+  const handleSubmitShow = (e) => {
+    setAgentSubmittedShow(true)
+  }
 
 
   return (
@@ -39,7 +150,7 @@ const NewHomepage = () => {
             <h1 className='main-message'>10x your leads and maximise your customer service</h1>
             <h4 className='secondary-message'>Wittle is a property companion for Estate Agents that uses data and AI to help you generate leads, save time and serve customers more effectively.</h4>
             <div className='cta-buttons'>
-              <button className='start'>Start trial</button>
+              <button className='start' onClick={handleAgentShow}>Get in touch</button>
               <button className='more'>Learn more</button>
             </div>
             <div className='top-line-benefits'>
@@ -83,7 +194,7 @@ const NewHomepage = () => {
           </div>
           <div className='cta-section'>
             <div className='cta-buttons'>
-              <button className='start'>Start trial</button>
+              <button className='start' onClick={handleAgentShow}>Get in touch</button>
               <button className='more'>Learn more</button>
             </div>
           </div>
@@ -118,7 +229,7 @@ const NewHomepage = () => {
               <div className='product-info'>
                 <h1 className='info-title'>Increase your market share with automated lead gen</h1>
                 <div className='cta-buttons'>
-                  <button className='start'>Start trial</button>
+                  <button className='start' onClick={handleAgentShow}>Get in touch</button>
                   <button className='more'>Learn more</button>
                 </div>
               </div>
@@ -129,7 +240,7 @@ const NewHomepage = () => {
               <div className='product-info'>
                 <h1 className='info-title'>Know everything about every postcode in London</h1>
                 <div className='cta-buttons'>
-                  <button className='start'>Start trial</button>
+                  <button className='start' onClick={handleAgentShow}>Get in touch</button>
                   <button className='more'>Learn more</button>
                 </div>
               </div>
@@ -140,7 +251,7 @@ const NewHomepage = () => {
               <div className='product-info'>
                 <h1 className='info-title'>Match customers with the perfect properties</h1>
                 <div className='cta-buttons'>
-                  <button className='start'>Start trial</button>
+                  <button className='start' onClick={handleAgentShow}>Get in touch</button>
                   <button className='more'>Learn more</button>
                 </div>
               </div>
@@ -151,15 +262,29 @@ const NewHomepage = () => {
               <div className='product-info'>
                 <h1 className='info-title'>Automate your listing generation</h1>
                 <div className='cta-buttons'>
-                  <button className='start'>Start trial</button>
+                  <button className='start' onClick={handleAgentShow}>Get in touch</button>
                   <button className='more'>Learn more</button>
                 </div>
               </div>
             </div>
           </div>
           <Footer textColour={'#ED6B86'} />
+          <AgentSignup
+            agentShow={agentShow}
+            handleAgentClose={handleAgentClose} handleAgenthandAgentShow submitError={submitError}
+            handleChange={handleChange}
+            handleSubmit={handleSubmit}
+            agentSubmitted={agentSubmitted}
+            handleSubmitShow={handleSubmitShow}
+          />
+          <AgentSubmitted
+            handleSubmitClose={handleSubmitClose}
+            agentSubmittedShow={agentSubmittedShow}
+            handleSubmitShow={handleSubmitShow}
+          />
 
         </section>
+
 
 
       </section>
